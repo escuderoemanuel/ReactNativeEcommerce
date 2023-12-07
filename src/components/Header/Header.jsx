34ca05792850d0296dd26599340638e9ef1d84bd
Emanuel Escudero <escudero.mdz@gmail.com>
2023-12-07 02:13:02 -0300

    Uninstalled Fonts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,54 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { colors } from '../../global/colors'
 
-import {
-  useFonts,
-  Poppins_100Thin,
-  Poppins_100Thin_Italic,
-  Poppins_200ExtraLight,
-  Poppins_200ExtraLight_Italic,
-  Poppins_300Light,
-  Poppins_300Light_Italic,
-  Poppins_400Regular,
-  Poppins_400Regular_Italic,
-  Poppins_500Medium,
-  Poppins_500Medium_Italic,
-  Poppins_600SemiBold,
-  Poppins_600SemiBold_Italic,
-  Poppins_700Bold,
-  Poppins_700Bold_Italic,
-  Poppins_800ExtraBold,
-  Poppins_800ExtraBold_Italic,
-  Poppins_900Black,
-  Poppins_900Black_Italic,
-} from '@expo-google-fonts/poppins';
-
 
 const Header = ({ title }) => {
-  const [fontsLoaded] = useFonts({
-    Poppins_100Thin,
-    Poppins_100Thin_Italic,
-    Poppins_200ExtraLight,
-    Poppins_200ExtraLight_Italic,
-    Poppins_300Light,
-    Poppins_300Light_Italic,
-    Poppins_400Regular,
-    Poppins_400Regular_Italic,
-    Poppins_500Medium,
-    Poppins_500Medium_Italic,
-    Poppins_600SemiBold,
-    Poppins_600SemiBold_Italic,
-    Poppins_700Bold,
-    Poppins_700Bold_Italic,
-    Poppins_800ExtraBold,
-    Poppins_800ExtraBold_Italic,
-    Poppins_900Black,
-    Poppins_900Black_Italic,
-  });
 
-  if (!fontsLoaded) {
-    return null;
-  }
 
   return (
     <View style={styles.header}>
@@ -70,7 +25,6 @@ const styles = StyleSheet.create({
   },
 
   headerText: {
-    fontFamily: 'Poppins_900Black',
     fontSize: 40,
     color: colors.paleGoldenRod,
   }
