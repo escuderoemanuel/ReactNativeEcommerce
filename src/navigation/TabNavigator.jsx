a9500837ae3cb48e8ec8e9fd3e3bbbfdf35653c6
Emanuel Escudero <escudero.mdz@gmail.com>
2023-12-19 23:27:53 -0300

    Updated TabNavigator Styles

--- src/navigation/TabNavigator.jsx
+++ src/navigation/TabNavigator.jsx
@@ -1,9 +1,10 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet } from "react-native";
 
 import { colors } from '../global/colors';
- 
+
 import ShopNavigator from './ShopNavigator';
 import CartNavigator from './CartNavigator';
 
@@ -19,8 +20,17 @@ const TabNavigator = () => {
           tabBarStyle: styles.tabBar,
         }}
       >
-        <Tab.Screen name="ShopStack" component={ShopNavigator} />
-        <Tab.Screen name="CartStack" component={CartNavigator} />
+        <Tab.Screen name="ShopStack" component={ShopNavigator} options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons name="storefront" style={focused ? styles.selected : styles.deselected} />
+          ),
+
+        }} />
+        <Tab.Screen name="CartStack" component={CartNavigator} options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons name="cart" style={focused ? styles.selected : styles.deselected} />
+          ),
+        }} />
       </Tab.Navigator>
     </NavigationContainer >
   )
@@ -30,5 +40,14 @@ export default TabNavigator;
 const styles = StyleSheet.create({
   tabBar: {
     backgroundColor: colors.darkBlue,
+  },
+  selected: {
+    fontSize: 28,
+    color: colors.paleGoldenRod,
+  },
+  deselected: {
+    fontSize: 24,
+    color: colors.greyLabel,
+
   }
 })
